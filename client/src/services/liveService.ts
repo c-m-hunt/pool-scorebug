c807943f0d159ad159d7ba76cbb75b1c72526ec3
Chris Hunt <chris.hunt@hudl.com>
2023-12-29 13:29:47 +0000

    feat: split services - only load web sockets for scorebug page

--- client/src/services/liveService.ts
+++ client/src/services/liveService.ts
@@ -1,47 +1,34 @@
 import { useEffect, useState } from "react";
 import { Scorebug } from "../types";
+import { ServiceResponse } from "./api";
 
 const baseUrl = "http://localhost:8080";
 
-interface ScorebugServiceResponse extends ServiceResponse {
+interface LiveScorebugServiceResponse extends ServiceResponse {
 	scorebug?: Scorebug;
-	saveScorebug: (scoerbug: Scorebug) => Promise<Scorebug>;
-	updateScorebug: (Scorebug: Scorebug) => void;
 }
 
-export interface ServiceResponse {
-	status: number;
-	loading: boolean;
-	error: Error | null;
-}
 
-export const useScorebugService = (live = false): ScorebugServiceResponse => {
+export const useLiveScorebugService = (live = false): LiveScorebugServiceResponse => {
 	const [scorebug, setScorebug] = useState<Scorebug | undefined>(undefined);
 	const [status, setStatus] = useState<number>(200);
 	const [loading, setLoading] = useState<boolean>(false);
 	const [error, setError] = useState<Error | null>(null);
 
-	const saveScorebug = async (scorebug: Scorebug) => {
-		setLoading(true);
-		try {
-			const [data, status] = await postApi("match", scorebug);
-			setScorebug(data);
-			setStatus(status);
-		} catch (err) {
-			console.error(err);
-			setError(err as Error);
-		} finally {
-			setLoading(false);
-		}
-		return scorebug;
-	};
-
-	const updateScorebug = (scorebug: Scorebug) => {
-		setScorebug(scorebug);
-	};
-
-    if (live) {}
 	useEffect(() => {
+		const ws = new WebSocket("ws://localhost:8080/ws");
+		ws.onmessage = (event) => {
+			const json = JSON.parse(event.data);
+			console.log(json);
+			try {
+				if (json.type === "scorebug") {
+					setScorebug(json.data);
+				}
+			} catch (err) {
+				console.log(err);
+			}
+		};
+
 		const getScorebug = async () => {
 			setLoading(true);
 			try {
@@ -56,6 +43,10 @@ export const useScorebugService = (live = false): ScorebugServiceResponse => {
 			}
 		};
 		getScorebug();
+
+		return () => {
+			ws.close();
+		};
 	}, []);
 
 	return {
@@ -63,8 +54,6 @@ export const useScorebugService = (live = false): ScorebugServiceResponse => {
 		status,
 		loading,
 		error,
-		saveScorebug,
-		updateScorebug,
 	};
 };
 
